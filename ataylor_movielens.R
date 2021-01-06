d1c44a7e008d5e2155d572d61698ac81c9ccff5c
adele-taylor <pthagonal@yahoo.co.uk>
2021-01-06 17:29:53 +0000

    wed update

--- ataylor_movielens.R
+++ ataylor_movielens.R
@@ -10,7 +10,10 @@
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
-
+if(!require(knitr)) install.packages("knitr", repos = "http://cran.us.r-project.org")
+if(!require(kableExtra)) install.packages("kableExtra", repos = "http://cran.us.r-project.org")
+library(knitr)
+library(kableExtra)
 library(tidyverse)
 library(caret)
 library(data.table)
@@ -63,7 +66,7 @@ length(unique(edx$movieId)) %>% print() #no. of movies
 # graph number of reviews per user
 median_user_reviews <- edx %>% group_by(userId) %>% summarise(n=n()) %>% pull() %>% median()
 png("user_reviews.png")
-user_reviews <- edx %>% group_by(userId) %>% summarise(n=n()) %>% ggplot(aes(n))+geom_histogram()+geom_vline(xintercept=median_user_reviews)
+user_reviews <- edx %>% group_by(userId) %>% summarise(n=n()) %>% ggplot(aes(n))+geom_histogram()+geom_vline(aes(xintercept=median_user_reviews, colour="Median"))+ggtitle("Number of reviews per user")+xlab("")+ylab("")+scale_color_manual(name = "", values = c(Median = "red")) 
 print(user_reviews)
 dev.off()
 browseURL("user_reviews.png")
@@ -71,7 +74,7 @@ browseURL("user_reviews.png")
 # graph number of reviews per film
 median_film_reviews <- edx %>% group_by(movieId) %>% summarise(n=n()) %>% pull() %>% median()
 png("movie_reviews.png")
-movie_reviews <- edx %>% group_by(userId) %>% summarise(n=n()) %>% ggplot(aes(n))+geom_histogram()+geom_vline(xintercept = median_film_reviews)
+movie_reviews <- edx %>% group_by(movieId) %>% summarise(n=n()) %>% ggplot(aes(n))+geom_histogram()+geom_vline(aes(xintercept = median_film_reviews, colour="Median"))+ggtitle("Number of reviews per film")+xlab("")+ylab("")+scale_colour_manual(name="", values=c(Median="red"))
 print(movie_reviews)
 dev.off()
 browseURL("movie_reviews.png")
@@ -79,27 +82,87 @@ browseURL("movie_reviews.png")
 # look at distribution of number of reviews vs average rating for films
 
 png("number_v_average_rating.png") 
-number_v_average_rating <- edx %>% group_by(movieId) %>% summarise(n=n(), average=mean(rating)) %>% mutate(number=ifelse(n<10, "<10 reviews", NA)) %>% ggplot(aes(n, average, col=number))+geom_point()
+number_v_average_rating <- edx %>% group_by(movieId) %>% summarise(n=n(), average=mean(rating)) %>% mutate(number=ifelse(n<10, "<10 reviews", ">=10 reviews")) %>% ggplot(aes(n, average, col=number))+geom_point()+ggtitle("Average rating vs number of reviews per film")+scale_colour_manual(name="", breaks=c('<10 reviews'), values=scales::hue_pal()(2))
 print(number_v_average_rating)
 dev.off()
 browseURL("number_v_average_rating.png") 
 
-# investigate possible associations using subset
+# investigate possible associations using subsets
 
 set.seed(20, sample.kind="Rounding")
 subset <- edx[sample(9000055, 10000),]
-subset2 <- subset %>% semi_join(edx, by="userId")
-subset2 <- subset2[sample(nrow(subset2),1000),]
 
+set.seed(10, sample.kind="Rounding")
+subset2 <- edx %>% semi_join(subset, by="userId")
+subset2 <- subset2[sample(nrow(subset2),10000),]
+
+
+#going to look at four basic models to start with
+models <- c("glm", "knn", "rf", "rpart")
 
 RMSE <- function(real, predicted){
-  sqrt(mean((real-predicted)^2))
+sqrt(mean((real-predicted)^2))
 }
 
+set.seed(3, sample.kind="Rounding")
+
+#frst userId alone
+userId_predictions <- sapply(models,function(model){
+fit<- train(rating ~ userId, model=model, data=subset)
+predictions <- predict(fit,subset2)
+RMSE(predictions, subset2$rating)
+})
+
+#then movieId alone
+set.seed(1, sample.kind="Rounding")
+subset3 <- edx[sample(9000055, 10000),]
+
+set.seed(8, sample.kind="Rounding")
+subset4 <- edx %>% semi_join(subset3, by="movieId")
+subset4 <- subset4[sample(nrow(subset4),10000),]
+
+set.seed(4, sample.kind="Rounding")
+
+movieId_predictions <- sapply(models,function(model){
+  fit<- train(rating ~ userId, model=model, data=subset3)
+  predictions <- predict(fit,subset4)
+  RMSE(predictions, subset4$rating)
+})
+
+
+#finally both
+
+set.seed(19, sample.kind="Rounding")
+subset5 <- edx[sample(9000055, 10000),]
+
+set.seed(80, sample.kind="Rounding")
+subset6 <- edx %>% semi_join(subset5, by="movieId")
+subset6 <- subset6[sample(nrow(subset6),10000),]
+
+set.seed(1, sample.kind="Rounding")
+
+user_and_movie_predictions <- sapply(models,function(model){
+  fit<- train(rating ~ userId, model=model, data=subset5)
+  predictions <- predict(fit,subset6)
+  RMSE(predictions, subset6$rating)
+})
+
+
+
+predictions <- userId_predictions %>% rbind(movieId_predictions) %>% rbind(user_and_movie_predictions)
+rownames(predictions) <- c("userId only", "movieId only", "both userId and movieId")
+
+predictions %>% kable() %>% kable_styling()
+
+test_index <- edx %>% createDataPartition(times = 1, p=0.1)
+test_set <- edx[index]
+train_set <- edx[-index] %>% semi_join(test_set, by="userId") %>% semi_join(test_set, by="movieId")
+
+###tune a regularised linear model
 
 lambdas <- seq(0, 10, 0.25)
 
-rmses <- sapply(lambdas, function(l){
+regularised <- sapply(lambdas, function(l){
 
   mu <- mean(train_set$rating)
 
@@ -121,3 +184,7 @@ rmses <- sapply(lambdas, function(l){
 
   return(RMSE(predicted_ratings, test_set$rating))
 })
+
+plot(lambdas,regularised)
+l<- lambdas[which(regularised==min(regularised))]
+min(regularised)
